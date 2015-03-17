3eb5a469547188b1d24523b345fa34d75922692d
Rommel Rico <rommeltj@gmail.com>
2015-03-17 08:23:17 -0700

    Initial Commit

---
+++ FlappyBird/GameScene.swift
@@ -0,0 +1,25 @@
+//
+//  GameScene.swift
+//  FlappyBird
+//
+//  Created by Rommel Rico on 3/17/15.
+//  Copyright (c) 2015 Rommel Rico. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    override func didMoveToView(view: SKView) {
+        /* Setup your scene here */
+        
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
+        
+    }
+   
+    override func update(currentTime: CFTimeInterval) {
+        /* Called before each frame is rendered */
+    }
+}
