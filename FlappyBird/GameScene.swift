a5e9b03cd5558c77c7dc270b9c0fdd5c511eec97
Rommel Rico <rommeltj@gmail.com>
2015-03-17 08:58:08 -0700

    Animating the background.

--- FlappyBird/GameScene.swift
+++ FlappyBird/GameScene.swift
@@ -10,9 +10,30 @@ import SpriteKit
 
 class GameScene: SKScene {
     var bird = SKSpriteNode()
+    var bg = SKSpriteNode()
     
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        
+        //Set the background.
+        var bgTexture = SKTexture(imageNamed: "FlappyBirdImages/bg.png")
+        var movebg = SKAction.moveByX(-bgTexture.size().width, y: 0, duration: 9)
+        var replaceBg = SKAction.moveByX(bgTexture.size().width, y: 0, duration: 0)
+        var moveBgForever = SKAction.repeatActionForever(SKAction.sequence([movebg, replaceBg]))
+
+        
+        for var i:CGFloat=0; i<3; i++ {
+            bg = SKSpriteNode(texture: bgTexture)
+            bg.position = CGPoint(x: bgTexture.size().width/2 + bgTexture.size().width * i, y: CGRectGetMidY(self.frame))
+            bg.size.height = self.frame.height
+            self.addChild(bg)
+            bg.runAction(moveBgForever)
+            
+       }
+        
+        
+        
+        
         //Set the bird textures.
         var birdTexture = SKTexture(imageNamed: "FlappyBirdImages/flappy1.png")
         var birdTexture2 = SKTexture(imageNamed: "FlappyBirdImages/flappy2.png")
